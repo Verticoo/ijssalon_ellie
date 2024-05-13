095ef2e67f1f61bbe66af1ae55f1a9b282d6c9b5
Verticoo <tim.reijnen84@gmail.com>
2024-05-13 14:11:06 +0200

    changed reclamme/algemenfuncties

---
+++ static/algemene_functies.py
@@ -0,0 +1,27 @@
+a = 2
+b = 4
+c = 10 
+d = 12
+e = 12.3
+f = 12.2
+g = 10.5
+f = 100.20
+def mijn_functie_1(a):
+      return a * a
+def mijn_functie_1(b):
+      return b * b
+def mijn_functie_1(c):
+      return c * c
+def mijn_fucntie_1(d):    
+      return d * d
+def mijn_functie_2(e):
+      return 12 + 3, 12 - 3, 12 * 3, 12 / 3 
+def mijn_functie_2(f):
+      return 12 + 2, 12 - 2, 12 * 2, 12 / 2 
+def mijn_functie_2 (g):
+      return 10 + 5, 10 - 5, 10 * 5, 10 /5
+def mijn_functie_2 (f):
+      return 100 + 20, 100 - 20, 100 * 20, 100 / 20
+
+print (mijn_fucntie_1(2))
+print (mijn_functie_2(100.20))
---
+++ static/reclame.py
@@ -0,0 +1,7 @@
+from algemene_functies import mijn_functie_2
+def combinatie(invoer_lijst_2):
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
+    return uitvoer
+ 
+
